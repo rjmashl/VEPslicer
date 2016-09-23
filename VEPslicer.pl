a0924471b928d6d87721cb56992137e76e6fb5c2
rjmashl <rjmashl@gmail.com>
2016-09-23 11:30:15 -0500

    add keyword verification

--- VEPslicer.pl
+++ VEPslicer.pl
@@ -1,8 +1,9 @@
-#!/usr/bin/env perl 
+#!/usr/bin/env perl
 
 # VEP Slicer: Extract variants from Variant Effect Predictor (VEP) annotated VCFs by keyword
 #
 # Author: R. Jay Mashl <rjmashl@gmail.com>
+# v0.3: add keyword verification
 # v0.2: handle multiple consequences
 # v0.1: initial version
 #
@@ -14,18 +15,18 @@ use warnings;
 
 my $vcf;          # VCF file
 my @label = ();     # list of VEP keywords
-my @fixed_col = (); # 0-based column index of VEP keywords 
+my $fixed_col;      # 0-based column index of VEP keywords
 
 sub get_value {
     my $info_str   = shift;
     my $field_name = shift;
-    
-    my @info_arr = split /\;/,$info_str;
+
+    my @info_arr = split( /\;/, $info_str );
     for(my $i = 0; $i < scalar @info_arr; $i++) {
-	if( index($info_arr[$i], $field_name) == 0) { # match start of string
-	    my @b = split /=/, $info_arr[$i];
-	    return $b[1];
-	}
+        if( index($info_arr[$i], $field_name) == 0) { # match start of string
+            my @b = split( /=/, $info_arr[$i] );
+            return $b[1];
+        }
     }
     return "";  # no match
 }
@@ -78,60 +79,68 @@ if( scalar @ARGV == 1) {
 
 
 ($vcf, @label) = @ARGV;
-$fixed_col[0] = -1;  # flag denoting column index is unassigned
 
 
+my $bVepSeen = 0; # check if have not seen VEP info
 open(IN , "< $vcf")  ||  die "\nError: Cannot find file $vcf\n\n\n";
 while(<IN>) {
     chomp;
-    if( $fixed_col[0] < 0 ) {   # check if have not seen VEP info
-	if(/^##INFO=<ID=CSQ/) {
-	    # get field map, e.g.
-            ##INFO=<ID=CSQ,Number=.,Type=String,Description="Consequence annotations from Ensembl VEP. Format: Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|DISTANCE|STRAND|VARIANT_CLASS|SYMBOL_SOURCE|HGNC_ID|CANONICAL|TSL|CCDS|ENSP|SWISSPROT|TREMBL|UNIPARC|SIFT|PolyPhen|DOMAINS|HGVS_OFFSET|GMAF|AFR_MAF|AMR_MAF|ASN_MAF|EAS_MAF|EUR_MAF|SAS_MAF|AA_MAF|EA_MAF|CLIN_SIG|SOMATIC|PHENO|PUBMED|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE">
-	    
-	    my @info_id = split /Format:\ /, $_;
-	    $info_id[1] = substr( $info_id[1], 0, -2 );  # remove end xml chars
-	    my @tmp     = split /\|/, $info_id[1];
-	    
-	    # searching/setting column in input order
-	    my $header_str = "#CHROM\tPOS\tREF\tALT";
-	    for( my $j = 0; $j < scalar @label ;  $j++) {
-		for( my $i = 0; $i < scalar @tmp ;  $i++) {
-		    if( $tmp[$i] eq $label[$j] ) {
-			$fixed_col[$j]  = $i;
-			$header_str    .= "\t".$tmp[$i];
-		    }
-		}
-	    }
-	    print $header_str, "\n";
-	}
-	else {
-	    next;
-	}
+    if( ! $bVepSeen ) {
+        if(/^##INFO=<ID=CSQ/) {
+            $bVepSeen = 1;
+            my @info_id = split /Format:\ /, $_;
+            $info_id[1] = substr( $info_id[1], 0, -2 );  # remove end xml chars
+            my @tmp     = split /\|/, $info_id[1];
+
+            # searching/setting column in input order
+            my $ptr = 0;
+            my $header_str = "#CHROM\tPOS\tREF\tALT";
+            for( my $j = 0; $j < scalar @label ;  $j++) {
+                my $bFound = 0;
+                for( my $i = 0; $i < scalar @tmp ;  $i++) {
+                    if( $tmp[$i] eq $label[$j] ) {
+                        $fixed_col->[$ptr++] = $i;
+                        $header_str    .= "\t".$label[$j];
+                        $bFound++;
+                        last;
+                    }
+                }
+                print "#Warning: requested keyword $label[$j] was not found.\n" if( ! $bFound );
+            }
+            if( $ptr ) {
+                print $header_str, "\n";
+            } else {
+                print "#Warning: no requested keywords were found. Exiting.\n";
+                exit;
+            }
+        }
+        else {
+            next;
+        }
     }
 
     # skip subsequent comments, if any
-    next if( /^#/ );   
+    next if( /^#/ );
 
     # process calls
     my @field = split /\t/, $_;
     my $value = get_value( $field[7], "CSQ" );
 
     if( length $value ) {  # CSQ was present
-	my %s = ();  # make outputs unique
-	foreach my $csq (split(/,/, $value)) { # allow multiple annotations
-	    my @anno_field = split /\|/, $csq, -1;
-	    my $print_str = join( "\t", @field[0,1,3,4] );
-	    for(my $j = 0 ; $j < scalar @fixed_col ; $j++) {
-		my $answer = $anno_field[ $fixed_col[$j] ];
-		if( length($answer) == 0 ) {
-		    $answer = "NULL";
-		}
-		$print_str .= "\t".$answer;
-	    }
-	    $s{$print_str} = 1;
-	}
-	foreach (keys %s) { print $_."\n"; }
+        my %s = ();  # make outputs unique
+        foreach my $csq (split(/,/, $value)) { # allow multiple annotations
+            my @anno_field = split /\|/, $csq, -1;
+            my $print_str = join( "\t", @field[0,1,3,4] );
+            for(my $j = 0 ; $j < scalar @{$fixed_col} ; $j++) {
+                my $answer = $anno_field[ $fixed_col->[$j] ];
+                if( length($answer) == 0 ) {
+                    $answer = "NULL";
+                }
+                $print_str .= "\t".$answer;
+            }
+            $s{$print_str} = 1;
+        }
+        foreach (keys %s) { print $_."\n"; }
     }
 }
 close( IN );
